a2d76762bf7ffbffd34e1764dc70b30ce5b56072
Alex(k$ei) Mishakov <44674940+amishakov@users.noreply.github.com>
2025-03-11 16:17:37 +0200

    Update install_arc_dsm.sh

--- install_arc_dsm.sh
+++ install_arc_dsm.sh
@@ -45,8 +45,8 @@ url="https://github.com/AuxXxilium/arc/releases/download/$version/arc-$version-e
 # Download and extract Arc image
 wget $url
 image_folder="/var/lib/vz/template/iso/"
-unzip "arc-$version-minimal.img.zip" -d $image_folder
-rm "arc-$version-minimal.img.zip"
+unzip "arc-$version-evo.img.zip" -d $image_folder
+rm "arc-$version-evo.img.zip"
 
 # Create virtual machine
 qm create "$VMID" --name DSM --memory 4096 --sockets 1 --cores 2 --cpu host --net0 virtio,bridge=vmbr0 --ostype l26
